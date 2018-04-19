0a56852a18bd90e73b6bbeef126e8412cd1c5e13
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-04-19 10:57:59 -0600

    updated configuration file

--- SHARMLib/CMake/SHARMLibConfig.cmake
+++ SHARMLib/CMake/SHARMLibConfig.cmake
@@ -20,30 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-if (EXISTS /home/bebe0705/.am_fortuna)
-	set(IS_FORTUNA ON)
-	message("-- This is Fortuna")
-
-else()
-	set(IS_FORTUNA OFF)
-endif()
-
-
-if(${IS_FORTUNA})
-	set(SHARMLIB_INCLUDE_HEADER /home/bebe0705/libs/local/include/)
-	set(SHARMLIB_LIBRARY /home/bebe0705/libs/local/lib/libSHARMLib.so)
-
-else()
-	set(SHARMLIB_INCLUDE_HEADER /usr/local/include/)
-
-	if (APPLE)
-		set(SHARMLIB_LIBRARY /usr/local/lib/libSHARMLib.dylib)
-	elseif(UNIX AND NOT APPLE)
-		set(SHARMLIB_LIBRARY /usr/local/lib/libSHARMLib.so)
-	else()
-		message(FATAL_ERROR "Unsupported platform")
-	endif()
-endif()
+set(SHARMLIB_INCLUDE_HEADER /home/bebe0705/libs/local/include/)
+set(SHARMLIB_LIBRARY /home/bebe0705/libs/local/lib/libSHARMLib.so)
 
 
 message("-- Found SHARMLib: " ${SHARMLIB_LIBRARY})
