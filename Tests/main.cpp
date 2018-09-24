8688fd3f42cd94a0cb7eea45d95be3c69817f64c
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-09-24 10:53:15 -0600

    updated CMakeLists.txt to reflect OMP support by clang 10

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -3,7 +3,7 @@
 
 int main(){
 
-	std::cout << "Found SHARMLib! \n";
+	std::cout << "Found SHARMLib \n";
 
 
 	arma::mat Cnm2f(1,1);
@@ -11,9 +11,6 @@ int main(){
 
 	int n_degree =0;
 	double ref_radius = 1;
-	double polygon_mass = 1;
-	double density = 1;
-	double total_mass = 1;
 
 	double r0[3] = {1,1,1};
 	double r1[3] = {1,1,1};
@@ -26,13 +23,12 @@ int main(){
 		Snm2f,
 		n_degree,
 		ref_radius,
-		polygon_mass,
-		density,
-		total_mass,
 		r0,
 		r1,
 		r2,                   
 		normalized);
+	std::cout << "Ran ComputePolyhedralCS \n";
+
 
 	return 0;
 }
